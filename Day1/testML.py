32089078fe3f9cdcf083d36f43ed62116f400735
Youngsoo1024 <youngsoo.kim.1982@gmail.com>
2025-12-18 13:54:24 -0500

    p11 on learning

--- Day1/testML.py
+++ Day1/testML.py
@@ -1,1 +1,18 @@
-print("hello world")
+import numpy as np
+ 
+import matplotlib.pyplot as plt
+
+# Data for plotting (optional, you can use lists as well)
+x_values = [1, 2, 3, 4]
+y_values = [1, 4, 2, 3]
+
+# Plot the data
+plt.plot(x_values, y_values)
+
+# Add labels and a title (optional customization)
+plt.xlabel('X-axis Label')
+plt.ylabel('Y-axis Label')
+plt.title('Simple Matplotlib Example')
+
+# Display the plot
+plt.show()
